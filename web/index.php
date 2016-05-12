1a5e801f69b8a44f18032bd6e04bd33e516e4fcd
davoge <davoge@users.noreply.github.com>
2016-05-11 22:14:59 -0700

    Update index.php

--- web/index.php
+++ web/index.php
@@ -4,10 +4,6 @@ require('../vendor/autoload.php');
 
 $app = new \Slim\Slim();
 
-$app->get('/', function () {
-    echo "Hello from Flynn on port " . $_ENV["PORT"] . " from container " . gethostname() . "\n";
-});
-
-$app->run();
+phpinfo();
 
 ?>
